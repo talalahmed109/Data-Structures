17925077f11e6d7a0acababcb8db8e3afe943486
Muhammad Zaeem Nasir <53820312+mzaeemnasir@users.noreply.github.com>
2021-01-14 16:38:23 +0500

    Update Menu.cpp

--- Project/Menu.cpp
+++ Project/Menu.cpp
@@ -1,55 +1,59 @@
 //Main menu
-
 #include"Menu.h"
 #include<string>
+#include<iomanip>
 
 void mainMenu()
 {
-    sf::RenderWindow window(sf::VideoMode(750, 512), "MyWindow",sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
-   
-    sf::Font font;
-    font.loadFromFile("Pinkerston.ttf");
-        //throw("Could not load Font\n");
+	sf::RenderWindow window(sf::VideoMode(750, 512), "Marquee Recommendation System", sf::Style::Close | sf::Style::Titlebar | sf::Style::Resize);
+	sf::Font font;
+	font.loadFromFile("Pinkerston.ttf");
+	//throw("Could not load Font\n");
 
-    sf::Text text;
-    text.setFont(font);
-    text.setCharacterSize(35);
-    text.setFillColor(sf::Color::White);
-    text.setString("MARQUEE RECOMMENDATION SYSTEM\n");
-    text.setPosition(105, 25);
+	sf::Text text;
+	text.setFont(font);
+	text.setCharacterSize(35);
+	text.setFillColor(sf::Color::White);
+	text.setString("MARQUEE RECOMMENDATION SYSTEM\n");
+	text.setPosition(105, 25);
 
-    sf::Text text1;
-    text1.setFont(font);
-    text1.setCharacterSize(35);
-    text1.setFillColor(sf::Color::Cyan);
-    text1.setString("1- Search Marquee by Name\n\n2- Search by Location\n\n3- Search by Price Range\n");
-    text1.setPosition(125, 100);
+	sf::Text text1;
+	text1.setFont(font);
+	text1.setCharacterSize(35);
+	text1.setFillColor(sf::Color::Cyan);
+	text1.setString("1- Search Marquee by Name\n\n2- Search by Location\n\n3- Search by Price Range\n");
+	text1.setPosition(125, 100);
 
-    while (window.isOpen())
-    {
+	while (window.isOpen())
+	{
 
-        window.clear(sf::Color::Black);
-        sf::Event evnt;
-        while (window.pollEvent(evnt))
-        {
-            if (evnt.type == evnt.Closed)
-            {
-                window.close();
-            }
-        }
-        window.draw(text);
-        window.draw(text1);
-        window.display();
-        int choice;
-        readData obj;
-        cout << "\n\t\tChoice: ";
-        cin >> choice;
-        if (choice == 1)
-        {
-            obj.read();
-            obj.display();
-        }
-    }
+		window.clear(sf::Color::Black);
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			if (evnt.type == evnt.Closed)
+			{
+				window.close();
+			}
+		}
+		window.draw(text);
+		window.draw(text1);
+		window.display();
+		int choice;
+		readData obj;
+		cout << "\n\t\tChoice: ";
+		cin >> choice;
+		if (choice == 1)
+		{
+			string marqeeName;
+			obj.read();
+			obj.display();
+			cin.ignore();
+			cout << "Enter the Marqee Name = ";
+			getline(cin, marqeeName);
+			obj.search_byName(marqeeName);
+		}
+	}
 }
 
 //void input()
@@ -73,54 +77,72 @@ void mainMenu()
 //}
 node::node()
 {
-    data = " ";
-    next = NULL;
+	data = " ";
+	next = NULL;
 }
 
 readData::readData()
 {
-    head = NULL;
-    tail = NULL;
+	head = NULL;
+	tail = NULL;
 }
 void readData::insert(string name)
 {
-    node* temp = new node;
+	node* temp = new node;
 
-    temp->data = name;
-    temp->next = NULL;
-    if (head == NULL)
-    {
-        head = temp;
-        tail = temp;
-        temp = NULL;
-    }
-    else
-    {
-        tail->next = temp;
-        tail = temp;
-    }
+	temp->data = name;
+	temp->next = NULL;
+	if (head == NULL)
+	{
+		head = temp;
+		tail = temp;
+		temp = NULL;
+	}
+	else
+	{
+		tail->next = temp;
+		tail = temp;
+	}
 }
 
 void readData::read()
 {
-    ifstream myfile("data.txt");
-    if (!myfile.is_open())
-    {
-        std::cout << "Unable to open file";
-    }
-    string line="";
-    while (getline(myfile, line))
-    {
-        insert(line);
-    }
+	ifstream myfile("data.txt");
+	if (!myfile.is_open())
+	{
+		std::cout << "Unable to open file";
+	}
+	string line = "";
+	while (getline(myfile, line))
+	{
+		insert(line);
+	}
 }
 void readData::display()
 {
-    node* temp = new node;
-    temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << "\t\n";
-        temp = temp->next;
-    }
+	node* temp = new node;
+	temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << "\t\n";
+		temp = temp->next;
+	}
+}
+void readData::search_byName(string Marquee)
+{
+
+	// Making A Temporary Node (pointing Head Of Linked List)
+	node* temp = head;
+	// Searching the Speacific 
+	while (temp != NULL)
+	{
+		if (temp->data == Marquee)
+		{
+			cout << "Marquee Found" << endl;
+			return;
+		}
+		temp = temp->next;
+	}
+	cout << "Marquee Not Found" << endl;
+	return;
 }
